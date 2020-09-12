df86cefcf1a63ff096e7881ba5fa8cf74af31d59
mizmarine <masa.guruto+marine@gmail.com>
2020-09-12 19:05:47 +0900

    .

--- src/main.go
+++ src/main.go
@@ -1,12 +1,21 @@
 package main
 
-import "hello-go-mock/src/entity"
+import (
+	"hello-go-mock/src/entity"
+	"hello-go-mock/src/repository"
+	"time"
+)
 
 func main() {
-
+	repo := repository.NewToDoWriterStdout()
+	todo := entity.ToDo{
+		Title: "mock tamesu yo",
+		Done:  false,
+		Limit: time.Now(),
+	}
+	repo.CreateToDo(todo)
 }
 
 type ToDoWriter interface {
 	CreateToDo(t entity.ToDo) error
 }
-
--- src/repository/todo.go.go
+++ src/repository/todo.go.go
@@ -1,3 +1,22 @@
 package repository
 
-type ToDoWriterStdout struct{}
+import (
+	"encoding/json"
+	"fmt"
+	"hello-go-mock/src/entity"
+)
+
+type toDoWriterStdout struct{}
+
+func NewToDoWriterStdout() *toDoWriterStdout {
+	return &toDoWriterStdout{}
+}
+
+func (s *toDoWriterStdout) CreateToDo(t entity.ToDo) error {
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(data))
+	return nil
+}
