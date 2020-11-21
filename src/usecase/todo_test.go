76b2959fb8bb8a07b269e1c771c7f6c41774c5fe
mizmarine <masa.guruto+marine@gmail.com>
2020-11-22 03:01:50 +0900

    multiple call test sample

--- src/usecase/todo_test.go
+++ src/usecase/todo_test.go
@@ -34,3 +34,58 @@ func TestToDoService_SaveTodo(t *testing.T) {
 
 	svc.SaveTodo(title)
 }
+
+func TestToDoService_SaveTodo_MultipleCall(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	writer := mock.NewMockToDoWriter(ctrl)
+
+	svc := ToDoService{
+		ToDoWriter: writer,
+	}
+
+	title1 := "test dayo1"
+	title2 := "test dayo2"
+	title3 := "test dayo3"
+
+	c1 := writer.
+		EXPECT().
+		CreateToDo(gomock.Any()).
+		Do(func(todo entity.ToDo) {
+			assert.Equal(t, title1, todo.Title)
+			assert.Equal(t, false, todo.Done)
+			return
+		}).
+		Return(nil).
+		Times(2)
+	c2 := writer.
+		EXPECT().
+		CreateToDo(gomock.Any()).
+		Do(func(todo entity.ToDo) {
+			assert.Equal(t, title2, todo.Title)
+			assert.Equal(t, false, todo.Done)
+			return
+		}).
+		Return(nil).
+		Times(3).
+		After(c1)
+	writer.
+		EXPECT().
+		CreateToDo(gomock.Any()).
+		Do(func(todo entity.ToDo) {
+			assert.Equal(t, title3, todo.Title)
+			assert.Equal(t, false, todo.Done)
+			return
+		}).
+		Return(nil).
+		Times(1).
+		After(c2)
+
+	// call method multiple times
+	svc.SaveTodo(title1)
+	svc.SaveTodo(title1)
+	svc.SaveTodo(title2)
+	svc.SaveTodo(title2)
+	svc.SaveTodo(title2)
+	svc.SaveTodo(title3)
+}
